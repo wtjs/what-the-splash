9b989f4f3b3fc35a56c6e7b29d9bd1e778b11d4c
Divyanshu Maithani <div.blackcat@gmail.com>
2018-08-03 11:57:19 +0530

    :fire: Init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Header from './components/Header/index';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => <header>Saga Splash</header>;
+
+export default Header;
