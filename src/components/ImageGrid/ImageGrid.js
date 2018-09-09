d6abfe19c33c75f2ea3a73f48c3d89617c55de18
Divyanshu Maithani <div.blackcat@gmail.com>
2018-09-09 22:42:08 +0530

    :zap: Add profiler

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, unstable_Profiler as Profiler } from 'react';
 import { connect } from 'react-redux';
 
 import Button from '../Button';
@@ -7,38 +7,56 @@ import { loadImages } from '../../actions';
 import './styles.css';
 
 class ImageGrid extends Component {
-    componentDidMount() {
-        this.props.loadImages();
-    }
+    logProfiler = (
+        profilerId,
+        phase,
+        actualTime,
+        baseTime,
+        startTime,
+        commitTime,
+    ) => {
+        console.log({
+            profilerId,
+            phase, // identify the phase of rendering or why the render was caused
+            actualTime, // time taken by react
+            baseTime, // time taken by react
+            startTime, // timestamp at which render started
+            commitTime, // timestamp at which render was committed to the renderer
+        });
+    };
 
     render() {
         const { isLoading, images, loadImages, error, imageStats } = this.props;
         return (
-            <div className="content">
-                <section className="grid">
-                    {images.map(image => (
-                        <div
-                            key={image.id}
-                            className={`item item-${Math.ceil(
-                                image.height / image.width,
-                            )}`}
-                        >
-                            <Stats stats={imageStats[image.id]} />
-                            <img
-                                src={image.urls.small}
-                                alt={image.user.username}
-                            />
-                        </div>
-                    ))}
-                </section>
-                {error && <div className="error">{JSON.stringify(error)}</div>}
-                <Button
-                    onClick={() => !isLoading && loadImages()}
-                    loading={isLoading}
-                >
-                    Load More
-                </Button>
-            </div>
+            <Profiler id="image-grid" onRender={this.logProfiler}>
+                <div className="content">
+                    <section className="grid">
+                        {images.map(image => (
+                            <div
+                                key={image.id}
+                                className={`item item-${Math.ceil(
+                                    image.height / image.width,
+                                )}`}
+                            >
+                                <Stats stats={imageStats[image.id]} />
+                                <img
+                                    src={image.urls.small}
+                                    alt={image.user.username}
+                                />
+                            </div>
+                        ))}
+                    </section>
+                    {error && (
+                        <div className="error">{JSON.stringify(error)}</div>
+                    )}
+                    <Button
+                        onClick={() => !isLoading && loadImages()}
+                        loading={isLoading}
+                    >
+                        Load More
+                    </Button>
+                </div>
+            </Profiler>
         );
     }
 }
